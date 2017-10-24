555086b2d14d42c0532ae3514ae7f3971e1116c5
Romy Misra <romymisra@gmail.com>
2017-10-24 16:51:30 -0700

    initial setup

--- server/dataSimulator.js
+++ server/dataSimulator.js
@@ -4,11 +4,40 @@ var faker = require('faker');
 var app = express();
 
 // create half a million user names 
-// users should be an array of objects[]
+var userGeneraton = function() {
+    var users = [];
+    var tempNames = {};
+    var publisherProb = 0.10;
+    var handle, name, number, timezone, pubBool;
+    while (users.length < 3) {
+        // generate name 
+        handle = faker.fake("{{name.firstName}} {{name.lastName}}").toLowerCase();
+        // generate username
+        name = handle.split(' ').join('');
+        // check if it's unique
+        if (!tempNames[handle]) {
+            tempNames[handle] = name;
+        // generate publisher value
+            number = Math.random();
+            if (number < publisherProb) {
+                pubBool = true;
+            }  else {
+                pubBool = false;
+            }
+            var userObj = {
+                handle: handle,
+                name: name, 
+                timezone: 'PST',
+                publisher: pubBool
+                }
+            users.push(userObj);
+        }
+        
+    }
+   // console.log('users', users);
+};
 
-for (var i= 0; i< 100; i++) {
-    
-}
+
 
 
 // create 10 million tweets 
