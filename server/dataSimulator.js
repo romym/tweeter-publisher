ef63d38e14d47e5d6121713c62165315c45a4e45
Romy Misra <romymisra@gmail.com>
2017-10-24 14:16:29 -0700

    minor changes

---
+++ server/dataSimulator.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var faker = require('faker');
+
+var app = express();
+
+// create half a million user names 
+// users should be an array of objects[]
+
+for (var i= 0; i< 100; i++) {
+    
+}
+
+
+// create 10 million tweets 
+
+app.listen(3000, function(){
+    console.log('listening on port 3000');
+})
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var faker = require('faker');
+
 var app = express();
 
 app.get('/tweets', function(req, res){
@@ -6,11 +8,21 @@ app.get('/tweets', function(req, res){
     res.send('Hello World');
 })
 
+
+
+// var arr = [];
+// for (var i = 0 ; i < 100000; i++) {
+//     var randomName = faker.name.findName()
+//     arr.push(randomName);
+// }
+// var uniqueItems = Array.from(new Set(arr))
+// console.log('uniquearray', uniqueItems.length);
+
+
 app.listen(3000, function(){
     console.log('listening on port 3000');
 })
 
 // write some tests
-// get this repo on github
 // have anther server which will send fake tweets to this one 
 // have a db file which create the schema and then write into postgres
